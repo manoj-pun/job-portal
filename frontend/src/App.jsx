964511748327d9ed21c3b4aa4b08e70e5b084b66
Manoj Pun <manojpun840@gmail.com>
2025-02-04 18:08:50 +0545

    added addJob Dashboard manageApplications and viewApplications

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,11 @@ import Applications from './pages/Applications'
 import RecruiterLogin from './components/RecruiterLogin'
 import { AppContext } from './context/AppContext'
 import Login from './components/Login'
+import Dashboard from './pages/Dashboard'
+import AddJob from './pages/AddJob'
+import ManageJob from './pages/ManageJob'
+import ViewApplication from './pages/ViewApplication'
+import "quill/dist/quill.snow.css"
 
 const App = () => {
 
@@ -19,6 +24,11 @@ const App = () => {
         <Route path="/" element={<Home/>} />
         <Route path="/apply-job/:id" element={<ApplyJob/>} />
         <Route path="/applications" element={<Applications/>} />
+        <Route path="/dashboard" element={<Dashboard/>}>
+          <Route path='add-job' element={<AddJob/>} />
+          <Route path='manage-job' element={<ManageJob/>} />
+          <Route path='view-applications' element={<ViewApplication/>} />
+        </Route>
       </Routes>
     </div>
   )
