90059b900f4d5e60cc2f7b4ae83651841c8238a1
Manoj Pun <manojpun840@gmail.com>
2025-02-17 11:02:16 +0545

    added getCompanyPostedJobs and changeVisibility

--- backend/controllers/companyController.js
+++ backend/controllers/companyController.js
@@ -147,5 +147,21 @@ export const changeJobApplicationStatus = async(req,res) => {
 
 //change job visibility
 export const changeVisibility = async(req,res) => {
-    
+    try {
+        const {id} = req.body
+
+        const companyId = req.company._id
+
+        const job = await jobModel.findById(id)
+
+        if(companyId.toString() === job.companyId.toString()){
+            job.visible = !job.visible
+        }
+
+        await job.save()
+
+        res.json({success:true,job})
+    } catch (error) {
+        res.json({success:false,message:error.message})
+    }
 }
