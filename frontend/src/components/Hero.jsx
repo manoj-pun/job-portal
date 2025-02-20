4298a6e2ae46fd3e761f871b9594977497b948ae
Manoj Pun <manojpun840@gmail.com>
2025-02-20 20:25:10 +0545

    added vercel.json

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -24,8 +24,8 @@ const Hero = () => {
   return (
     <div className='container 2xl:px-20 mx-auto my-6'>
       <div className='bg-gradient-to-r from-green-600 to-green-800 text-white py-14 text-center mx-2 rounded-xl'>
-        <h2 className='text-2xl md:text-3xl lg:text-4xl font-medium mb-4'>Over 10,000+ jobs to apply</h2>
-        <p className='mb-8 max-w-xl mx-auto text-sm font-light px-5'>Your Next Big Career Move Starts Right Here - Explore the Best Job Opportunities and Take the First Step Toward Your Future!</p>
+        <h2 className='text-2xl md:text-3xl lg:text-4xl font-medium mb-4'>Unlock Your Next Career Move!</h2>
+        <p className='mb-8 max-w-xl mx-auto text-xl font-light px-5'>Discover Endless Career Possibilities and Take the First Step Toward Your Dream Job Today!</p>
         <div className='flex items-center justify-between bg-white rounded text-gray-600 max-w-xl pl-4 mx-4 sm:mx-auto'>
             <div className='flex items-center'>
                 <img className='h-4 sm:h-5' src={assets.search_icon} alt="" />
